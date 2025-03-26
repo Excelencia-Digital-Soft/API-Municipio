14ddd51eb7b5a4fbca55d0ee739b722ce44e63d7
Federico <federicodecomercio@gmail.com>
2025-03-26 10:12:08 -0300

    Agregado token a swagger

--- HCWebAPI/Program.cs
+++ HCWebAPI/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity;
 using Models.Municipalidad;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +18,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//JWT
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. 
+                    Enter 'Bearer' [space] and then your token in the text input below.
+                    Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "bearer",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+        },
+        new List<string>()
+    }
+});
+
+});//JWT
 builder.Services.AddAuthentication(opcions =>
 {
     opcions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
